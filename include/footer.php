eb31e6414566d55d8c5ddce41a5c68f1f7f5debb
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-11 21:51:03 +0000

    Updated header for js file, added design credit to footer

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
 </div>
 <div id="footer" class="fixed">
 	<?php if(!$_SESSION['callsign'])$name="Guest";
-	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> - &copy; Copyright 2010 BZBureau. All rights reserved.
+	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> - Design by NodeThirtyThree - &copy; Copyright 2010 BZBureau.com
 </div>
 </body>
 </html>
