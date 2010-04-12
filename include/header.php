6dc60ffdd4382aea00598991bc2ec5bab7420af6
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-12 21:07:15 +0000

    Added LGPL Disclamer and information at the top of each PHP file.

--- include/header.php
+++ include/header.php
@@ -1,3 +1,26 @@
+<?php
+/*
+    BZWeb v1.0
+    Copyright (c) 2010 Tony Bruess
+
+	BZWeb is an online based tool developed by mrapple which allows multiple users to manage bzfs instances.
+	For questions, join ##bzbureau on irc.freenode.net and ask mrapple.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
--- include/session.php
+++ include/session.php
@@ -1,4 +1,27 @@
 <?php
+/*
+    BZWeb v1.0
+    Copyright (c) 2010 Tony Bruess
+
+	BZWeb is an online based tool developed by mrapple which allows multiple users to manage bzfs instances.
+	For questions, join ##bzbureau on irc.freenode.net and ask mrapple.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not, see
+    <http://www.gnu.org/licenses/>.
+*/
+?>
+<?php
 session_start(); 
 header("Cache-control: private");
 include("global/vars.php");
