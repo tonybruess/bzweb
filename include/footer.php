b7cbf81a5bd3ecf74635f4757b6712c7cbc6f3da
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-13 01:39:41 +0000

    Edited footer Added new login system, displays info page instead of automatic redirect Edited menu for new login system Added basic file structure Added reference material for how to create mysql tables

--- include/footer.php
+++ include/footer.php
@@ -24,7 +24,7 @@
 	</div>
 	
 	<div id="PageBottom">
-		<div id="Copyright"><span>	<?php if(!$_SESSION['callsign'])$name="Guest";
+		<div id="Copyright"><span><?php if(!$_SESSION['callsign'])$name="Guest";
 	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> &copy; 2010 BZBureau.com<span></div>
 	</div>
 
