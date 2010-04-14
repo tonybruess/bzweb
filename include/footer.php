14e0e2029fae0eb9f4cf871beed2c07710a264db
circuit.spider <circuit.spider@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-14 16:48:42 +0000

    closed an open span tag

--- include/footer.php
+++ include/footer.php
@@ -25,7 +25,7 @@
 	
 	<div id="PageBottom">
 		<div id="Copyright"><span><?php if(!$_SESSION['callsign'])$name="Guest";
-	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> &copy; 2010 BZBureau.com<span></div>
+	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> &copy; 2010 BZBureau.com</span></div>
 	</div>
 
 </div>
