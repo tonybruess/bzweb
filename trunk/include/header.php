48566da1ece182b0df15ee855cacbfe7e0b26c38
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-15 21:36:28 +0000

    Styleswitcher nolonger needed xml tag not supported by html4

--- trunk/include/header.php
+++ trunk/include/header.php
@@ -22,7 +22,7 @@
 */
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html>
 
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -30,7 +30,6 @@
 	<link rel="stylesheet" type="text/css" href="global/servers.css">
 	<script src="global/global.js" type="text/javascript"></script>
 	<script src="global/servers.js" type="text/javascript"></script>
-	<script src="global/styleswitch.js" type="text/javascript"></script>
 	<title>BZWeb</title>
 </head>
 
