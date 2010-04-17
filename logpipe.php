4931f91c71498a43d07cc092d5368d4ff5070e7e
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-04-17 16:58:30 +0000

    Logpipe outline - in testing

---
+++ logpipe.php
@@ -0,0 +1,88 @@
+<?php
+
+chdir("../../");
+mkdir("test22");
+require "include/mysql.php";
+set_time_limit(0);
+stream_set_timeout(STDIN,0);
+stream_set_blocking(STDIN,0);
+
+// we need to ping MySQL every 10 minutes to keep it alive
+$lastPing = time();
+
+	file_put_contents("logpipe.log", "something here");
+// make sure the server id is specified
+if($argc < 2 || ! is_numeric($argv[1]))
+	die("A server id must be specified.\n");
+
+// continuously read from STDIN and pipe to the log database
+while(! feof(STDIN)) {
+	// attempt to read from STDIN
+	$line = trim(fgets(STDIN));
+
+	// if no data, make sure we at least ping the database every now and then
+	if(strlen($line) == 0) {
+		if($lastPing < time() - 600) {
+			mysql_query("DO 0");
+			$lastPing = time();
+
+		}
+
+		sleep(1);
+		continue;
+	}
+
+	$entities = array();
+	preg_match("/(\d+)-(\d+)-(\d+)\s(\d+):(\d+):(\d+):\s(\w+(-\w+)?)\s(.*)/",$line,$entities);
+	// formate the timestamp
+	$ts = mktime($entities[4],$entities[5],$entities[6],$entities[2],$entities[3],$entities[1]);
+
+	if(count($entities) > 1)
+		mysql_query("INSERT INTO serverlogs SET ".
+				"server=".$argv[1].",".
+				"time=".$ts.",".
+				"type=\"".mysql_real_escape_string($entities[7])."\",".
+				"data=\"".mysql_real_escape_string($entities[9])."\"");
+	else if(preg_match("/\S/",$line) == 1)
+		// if we can't parse it and it's something other than whitespace, log it
+		fwrite(STDERR,"Unable to parse server log entry: ".$line."\n");
+
+	// do other logic depending on entry type
+	if($entities[7] == "PLAYER-JOIN") {
+		// also enter players into players table, avoiding duplicates
+		$player = array();
+		preg_match("/^\d+:(.*)\s#\d+\s(BZid:(\d*)\s)?\w+\sIP:(\d+(\.\d+)+)\s?(\w+(\s\w+)*)?$/",$entities[9],$player);
+
+		if(count($player) == 0)
+			fwrite(STDERR,"Could not parse player join: ".$entities[9]."\n");
+
+		$oldrecord = mysql_query("SELECT * FROM players WHERE ".
+				"name=\"".$player[1]."\" AND ".
+				"ip=\"".$player[4]."\" AND ".
+				"description".($player[6] != "" ? "=\"".$player[6]."\"" : " IS NULL"));
+		if(count($oldrecord) > 0) {
+			// verify the existing record to make sure the last seen time are up-to-date
+			if($oldrecord['time'] < $ts) {
+				mysql_query("UPDATE players SET ".
+						"time=".$ts." WHERE ".
+						"name=\"".$player[1]."\" AND ".
+						"ip=\"".$player[4]."\" AND ".
+						"description".($player[6] != "" ? "=\"".$player[6]."\"" : " IS NULL"));
+
+			}
+		} else {
+			// insert a new player record
+			mysql_query("INSERT INTO players SET ".
+					"name=\"".$player[1]."\"".
+					",ip=\"".$player[4]."\"".
+					",host=\"".gethostbyaddr($player[4])."\"".
+					($player[6] != "" ? ",description=\"".$player[6]."\"" : "").
+					($player[3] != "" ? ",bzid=".$player[3] : "").
+					",time=".$ts);
+		}
+	}
+}
+
+exit;
+
+?>
