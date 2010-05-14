343565b2a0f92a530da57c5080e0bf5dbe640db3
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-05-14 23:09:30 +0000

    Require JavaScript Updated files page for ban files New JavaScript functions make it easier to hide/show form elements

--- trunk/include/footer.php
+++ trunk/include/footer.php
@@ -21,14 +21,14 @@
     <http://www.gnu.org/licenses/>.
 */
 ?>
-	</div>
+		</div>
 	
-	<div id="PageBottom">
-		<div id="Copyright"><span><?php if(!$_SESSION['callsign'])$name="Guest";
-	echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> &copy; 2010 BZBureau.com</span></div>
+		<div id="PageBottom">
+			<div id="Copyright"><span><?php if(!$_SESSION['callsign'])$name="Guest";
+		echo 'Logged in as '.$name.' from '.$_SERVER['REMOTE_ADDR']?> &copy; 2010 BZBureau.com</span></div>
+		</div>
+
 	</div>
-
 </div>
-
 </body>
 </html>
