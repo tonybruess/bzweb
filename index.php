3a2cf417c4ecca437a6975d65608e7fdd57daae1
yassen.bz <yassen.bz@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-06-06 23:16:12 +0000

    Fix a possible security hole and silence some notices.

--- index.php
+++ index.php
@@ -1,34 +1,51 @@
 <?php
-if(!file_exists("include/mysql.php")){
-	include_once("include/install.php");
+require('./include/security.php');
+include('./include/session.php');
+
+if(!file_exists("./include/mysql.php"))
+{
+	include_once("./include/install.php");
 	die();
 }
-if(file_exists("include/update.php")){
-	include_once("include/update.php");
+if(file_exists("./include/update.php"))
+{
+	include_once("./include/update.php");
 	die();
 }
-session_start(); 
-header("Cache-control: private");
-$name = $_SESSION['callsign'];
-if(!$_SESSION['callsign'] && $_GET['p'] !== "error"){
-	include_once("include/header.php");
-	include_once("include/menu.php");
-?>
+
+if(!isset($_SESSION['callsign']) && isset($_GET['p']) && $_GET['p'] != 'error')
+{
+	include_once("./include/header.php");
+	include_once("./include/menu.php");
+	
+	?>
 		<h3>Please login</h3>
-		<p>Before accessing this page you must <a href="http://my.bzflag.org/weblogin.php?url=http://<?php echo $_SERVER['HTTP_HOST']; ?>%2Fauthenticate.php%3Ftoken%3D%25TOKEN%25%26username%3D%25USERNAME%25">login.</a></p>
+		<p>
+			Before accessing this page you must <a href="http://my.bzflag.org/weblogin.php?url=http://<?php echo $_SERVER['HTTP_HOST']; ?>%2Fauthenticate.php%3Ftoken%3D%25TOKEN%25%26username%3D%25USERNAME%25">login.</a>
+		</p>
 	<?php
-	include_once("include/footer.php");
-	} else {
-	include_once("include/header.php");
-	$page = $_GET['p'];
-	if($page && file_exists("pages/$page.pag")){
-		include_once("include/menu.php");
-		include_once("pages/$page.pag");
-	} else {
-		$page = 'index';
-		include_once("include/menu.php");
-		include_once("pages/index.pag");
+	
+	include_once("./include/footer.php");
+}
+else
+{
+	include_once("./include/header.php");
+	
+	if(!isset($_GET['p']))
+		$_GET['p'] = 'index';
+		
+	$page = CleanFilePath($_GET['p']);
+	if($page === true && file_exists("./pages/$page.pag"))
+	{
+		include_once("./include/menu.php");
+		require_once("./pages/$page.pag");
 	}
-		include_once("include/footer.php");
+	else
+	{
+		$page = 'index';
+		include_once("./include/menu.php");
+		include_once("./pages/index.pag");
+	}
+	include_once("./include/footer.php");
 }
 ?>
