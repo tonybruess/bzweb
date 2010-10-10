d7ca3fcc59f68f44c28b7cd2c22c36b90daf5097
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-10-10 16:57:45 +0000

    Config.php is now configuration file, instead of mysql.php

---
+++ trunk/include/config.default.php
@@ -0,0 +1,6 @@
+/* This is the MySQL Configuration File */
+
+define('SQL_SERVER','localhost'); 
+define('SQL_USER','mrapple'); 
+define('SQL_PASS',''); 
+define('SQL_DB','bzweb'); 
--- trunk/include/mysql.php
+++ trunk/include/mysql.php
@@ -34,11 +34,9 @@
 		}
 		return $str ;
 	}
-    define('SQL_SERVER','localhost'); 
-    define('SQL_USER','mrapple'); 
-    define('SQL_PASS',''); 
-    define('SQL_DB','bzweb'); 
-        
+
+	include_once("../config.php");
+	        
     // Creating the connection using the above configuration
     mysql_connect(SQL_SERVER,SQL_USER,SQL_PASS) or die("Error: ".mysql_error()); // Connecting to the server
     mysql_select_db(SQL_DB) or die("Error: ".mysql_error()); // Connecting to the database
