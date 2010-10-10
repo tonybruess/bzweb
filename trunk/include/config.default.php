44583da25bc90f47aae24b50b5fa8e79a27cd98b
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-10-10 17:01:36 +0000

    Bug fix

--- trunk/include/config.default.php
+++ trunk/include/config.default.php
@@ -1,6 +1,8 @@
+<?php
 /* This is the MySQL Configuration File */
 
-define('SQL_SERVER','localhost'); 
-define('SQL_USER','mrapple'); 
-define('SQL_PASS',''); 
-define('SQL_DB','bzweb'); 
+define('SQL_SERVER','localhost');
+define('SQL_USER','mrapple');
+define('SQL_PASS','');
+define('SQL_DB','bzweb');
+?>
