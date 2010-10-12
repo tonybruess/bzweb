597d8a6097dacc52dc814fb97a29e6bb40b0edd6
tonybruess <tonybruess@beaf1eb3-337f-0561-ff7c-a99e6acf3c91>
2010-10-12 23:54:50 +0000

    Mostly code cleanup Logpipe updates Require fixes (Pages, your next ;))

--- trunk/index.php
+++ trunk/index.php
@@ -1,34 +1,30 @@
 <?php
 /*
-    BZWeb v1.0
-    Copyright (c) 2010 Tony Bruess
+	BZWeb v1.0
+	Copyright (c) 2010 Tony Bruess
 
 	BZWeb is an online based tool developed by mrapple which allows multiple users to manage bzfs instances.
 	For questions, join #bzextreme on irc.freenode.net and ask mrapple.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this program.  If not, see
-    <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Lesser General Public
+	License along with this program.  If not, see
+	<http://www.gnu.org/licenses/>.
 */
-
-$name = $_SESSION['callsign'];
 
 require('./config.php');
 require('./include/session.php');
 require('./include/mysql.php');
 require('./include/security.php');
-
-/* We're just gonna sanitize everything. */
 
 foreach ($_POST as $key => $value) { 
 	$_POST[$key] = sanitize($value); 
@@ -36,47 +32,40 @@ foreach ($_POST as $key => $value) {
 
 $name = $_SESSION['callsign'];
 $authPage = urlencode('http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']).'/authenticate.php?token=%TOKEN%&username=%USERNAME%');
+$page = CleanFilePath($_GET['p']);
+
+if(!$page)
+	$page = 'index';
 
 if(!file_exists('./config.php'))
 {
 	require_once('./include/install.php');
+	require_once('./include/footer.php');
 	die();
 }
 
 if(file_exists('./include/update.php'))
 {
 	require_once('./include/update.php');
+	require_once('./include/footer.php');
 	die();
 }
 
+require_once('./include/header.php');
+require_once('./include/menu.php');	
+
 if(!isset($_SESSION['callsign']) && $_GET['p'] != 'error')
 {
-	require_once('./include/header.php');
-	require_once('./include/menu.php');	
-?>
-		<h3>Please login</h3>
-		<p>Before accessing this page you must <a href='http://my.bzflag.org/weblogin.php?url=<?php echo $authPage; ?>'>login.</a></p>
-<?php
-	require_once("./include/footer.php");
+	require_once('./pages/login.php');
 }
 else
-{
-	require_once("./include/header.php");
-	
-	if(!isset($_GET['p']))
-		$_GET['p'] = 'index';
-		
-	$page = CleanFilePath($_GET['p']);
-	
-	if($page == true && file_exists("./pages/$page.php"))
+{	
+	if(ile_exists("./pages/$page.php"))
 	{
-		require_once("./include/menu.php");
 		require_once("./pages/$page.php");
 	}
 	else
 	{
-		$page = 'index';
-		require_once("./include/menu.php");
 		require_once("./pages/index.php");
 	}
 	require_once("./include/footer.php");
--- trunk/logpipe.php
+++ trunk/logpipe.php
@@ -1,24 +1,24 @@
 <?php
 /*
-    BZWeb v1.0
-    Copyright (c) 2010 Tony Bruess
+	BZWeb v1.0
+	Copyright (c) 2010 Tony Bruess
 
 	BZWeb is an online based tool developed by mrapple which allows multiple users to manage bzfs instances.
 	For questions, join #bzextreme on irc.freenode.net and ask mrapple.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this program.  If not, see
-    <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Lesser General Public
+	License along with this program.  If not, see
+	<http://www.gnu.org/licenses/>.
 */
 
 /* Thanks Constitution!! */
@@ -26,105 +26,110 @@
 chdir("../../");
 $name = $argv[3];
 
-// Connect to mysql
-require "config.php";
+require('./config.php');
 
-mysql_connect(SQL_SERVER,SQL_USER,SQL_PASS) or die("Error: ".mysql_error()); // Connecting to the server
-mysql_select_db(SQL_DB) or die("Error: ".mysql_error()); // Connecting to the database
+mysql_connect(SQL_SERVER,SQL_USER,SQL_PASS) or die("Error: ".mysql_error());
+mysql_select_db(SQL_DB) or die("Error: ".mysql_error());
 
-$stdin=fopen('php://stdin','r');
-$stderr=fopen('php://stderr','r');
+$stdin = fopen('php://stdin', 'r');
+$stderr = fopen('php://stderr', 'r');
 
 set_time_limit(0);
 
 stream_set_timeout($stdin,0);
 stream_set_blocking($stdin,0);
 
-// we need to ping MySQL every 10 minutes to keep it alive
 $lastPing = time();
-
-// Use $argc and $argv from $_SERVER, thanks blast
 $argc = $_SERVER['argc'];
 $argv = $_SERVER['argv'];
 
-// make sure the server id is specified
 if($argc < 2 || ! is_numeric($argv[1]))
 	die("A server id must be specified.\n");
+	
+function printError($text)
+{
+	fwrite($stderr,$text);
+	return true;
+}
 
-// continuously read from $stdin and pipe to the log database
-while(! feof($stdin)) {
-	// attempt to read from $stdin
+while(! feof($stdin))
+{
 	$line = trim(fgets($stdin));
 
-	// if no data, make sure we at least ping the database every now and then
-	if(strlen($line) == 0) {
-		if($lastPing < time() - 600) {
+	if(strlen($line) == 0)
+	{
+		if($lastPing < time() - 600)
+		{
 			mysql_query("DO 0");
 			$lastPing = time();
-
 		}
-
 		sleep(1);
 		continue;
 	}
 
 	$entities = array();
 	preg_match("/(\d+)-(\d+)-(\d+)\s(\d+):(\d+):(\d+):\s(\w+(-\w+)?)\s(.*)/",$line,$entities);
-	// formate the timestamp
+	
+	// entities[1] = Hour
+	// entities[2] = Minute
+	// entities[3] = Second
+	// entities[4] = Month
+	// entities[5] = Day
+	// entities[6] = Year
+	
 	$ts = mktime($entities[4],$entities[5],$entities[6],$entities[2],$entities[3],$entities[1]);
 
 	if(count($entities) > 1)
-		mysql_query("INSERT INTO ".$argv[1]."serverlogs SET "."server=".$argv[1].","."time=".$ts.","."type=\"".mysql_real_escape_string($entities[7])."\","."data=\"".mysql_real_escape_string($entities[9])."\"");
-	else if(preg_match("/\S/",$line) == 1)
-		// if we can't parse it and it's something other than whitespace, log it
-		fwrite($stderr,"Unable to parse server log entry: ".$line."\n");
+	{
+		$type = mysql_real_escape_string($entities[7]);
+		$data = mysql_real_escape_string($entities[9]);
+		mysql_query("INSERT INTO ".$argv[1]."serverlogs SET `server`='$argv[1]', `time`='$ts', `type`='$type',`data`='$data'");
+	}
+	elseif(preg_match("/\S/",$line) == 1)
+	{
+		printError("Unable to parse server log entry: ".$line."\n");
+	}
 
-	// do other logic depending on entry type
-	if($entities[7] == "PLAYER-JOIN") {
-		// also enter players into players table, avoiding duplicates
+	if($entities[7] == "PLAYER-JOIN")
+	{
 		$player = array();
 		preg_match("/^\d+:(.*)\s#\d+\s(BZid:(\d*)\s)?\w+\sIP:(\d+(\.\d+)+)\s?(\w+(\s\w+)*)?$/",$entities[9],$player);
 
 		if(count($player) == 0)
-			fwrite($stderr,"Could not parse player join: ".$entities[9]."\n");
+			printError("Could not parse player join: ".$entities[9]."\n");
 
-		$q = mysql_query("SELECT * FROM players WHERE `name`='$player[1]' AND `ip`='$player[4]' AND `description`='$player[6]'");
-		$oldrecord = mysql_fetch_array($q);
-		if($oldrecord[0]) {
-			// verify the existing record to make sure the last seen time are up-to-date
-			if($oldrecord['time'] < $ts) {
-				mysql_query("UPDATE players SET ".
-						"time=".$ts." WHERE ".
-						"name=\"".$player[1]."\" AND ".
-						"ip=\"".$player[4]."\" AND ".
-						"serverowner=\"".$name."\" AND ".
-						"description".($player[6] != "" ? "=\"".$player[6]."\"" : " IS NULL"));
-			}
-		} else {
-			// insert a new player record
+		$oldrecord = mysql_fetch_array(mysql_query("SELECT * FROM players WHERE `name`='$player[1]' AND `ip`='$player[4]' AND `description`='$player[6]'"));
+		if($oldrecord[0])
+		{
+			if($oldrecord['time'] < $ts)
+				mysql_query("UPDATE players SET `time`='$ts' WHERE `name`='$player[1]' AND `ip`='$player[4]' AND `serverowner`='$name' AND `description`='$player[6]'");
+		}
+		else
+		{
 			$host = gethostbyaddr($player[4]);
 			mysql_query("INSERT INTO players SET `serverowner`='$name', `name`='$player[1]',`ip`='$player[4]',`host`='$host',`description`='$player[6]',`bzid`='$player[3]',`time`='$ts'");
 		}
 	}
-	if($entities[7] == 'MSG-COMMAND' && preg_match("/^\d+:(.*?) ban (^\d+$.^\d+$.^\d+$.^\d+$|(.*?)) ([^ ]*) (.*)/",$entities[9],$ban)){
-		if(is_numeric($ban[2]) && is_numeric($ban[3]) && is_numeric($ban[4]) && is_numeric($ban[5])){
-		mysql_query("INSERT INTO bans SET `server` = '$argv[1]', `banner`= '$ban[1]', `ip` = '$ban[2].$ban[3].$ban[4].$ban[5]', `length` = '$ban[6]', `reason` = '$ban[7]', `time` = '$ts'");
-		} else {
-					mysql_query("INSERT INTO bans SET `server` = '$argv[1]', `banner`= '$ban[1]', `ip` = '$ban[2]', `length` = '$ban[4]', `reason` = '$ban[5]', `time` = '$ts'");
-		}
+	if($entities[7] == 'MSG-COMMAND' && preg_match("/^\d+:(.*?) ban (^\d+$.^\d+$.^\d+$.^\d+$|(.*?)) ([^ ]*) (.*)/",$entities[9],$ban))
+	{
+		if(is_numeric($ban[2]) && is_numeric($ban[3]) && is_numeric($ban[4]) && is_numeric($ban[5]))
+			mysql_query("INSERT INTO bans SET `server` = '$argv[1]', `banner`= '$ban[1]', `ip` = '$ban[2].$ban[3].$ban[4].$ban[5]', `length` = '$ban[6]', `reason` = '$ban[7]', `time` = '$ts'");
+		else
+			mysql_query("INSERT INTO bans SET `server` = '$argv[1]', `banner`= '$ban[1]', `ip` = '$ban[2]', `length` = '$ban[4]', `reason` = '$ban[5]', `time` = '$ts'");
 	}
-	if($entities[7] == 'MSG-REPORT' && preg_match("/(.*?):(.*?$)/",$entities[9],$report)){
-         $justmessage = substr($report[2],$report[1]);
-         $entirelength = strlen($report[2]);
-         $actuallength = ($entirelength - $report[1]);
-         $justuser = substr($report[2],0,-$actuallength);
-		mysql_query("INSERT INTO reports SET `server` = '$argv[1]', `serverowner` = '$name', `reporter`= '$justuser', `report` = '$justmessage', `time` = '$ts'");
+	if($entities[7] == 'MSG-REPORT' && preg_match("/(.*?):(.*?$)/",$entities[9],$report))
+	{
+		 $justmessage = substr($report[2],$report[1]);
+		 $entirelength = strlen($report[2]);
+		 $actuallength = ($entirelength - $report[1]);
+		 $justuser = substr($report[2],0,-$actuallength);
+		 mysql_query("INSERT INTO reports SET `server` = '$argv[1]', `serverowner` = '$name', `reporter`= '$justuser', `report` = '$justmessage', `time` = '$ts'");
 	}
-
+	printError(mysql_error());
 }
-mysql_query("DELETE FROM ".$argv[1]."serverlogs WHERE `type`='SERVER-STATUS' AND `data`='Running' AND `time`='$ts'");
-//Insert into logs that the server is done
-mysql_query("INSERT INTO ".$argv[1]."serverlogs SET `server` = '$argv[1]', `type` = 'SERVER-STATUS', `data` = 'Stopped', `time` = '$ts'");
 
+mysql_query("DELETE FROM ".$argv[1]."serverlogs WHERE `type`='SERVER-STATUS' AND `data`='Running' AND `time`='$ts'");
+mysql_query("INSERT INTO ".$argv[1]."serverlogs SET `server` = '$argv[1]', `type` = 'SERVER-STATUS', `data` = 'Stopped', `time` = '$ts'");
+printError("Exiting");
 exit;
 ?>
